9108d317ae28da516ca4676311fcc09a6a430b6a
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-24 00:18:43 +0530

    Candidate Repository Impl and Service Layer

---
+++ src/main/java/com/org/serviceImpl/CandidateServiceImpl.java
@@ -0,0 +1,25 @@
+package com.org.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.org.entity.Candidate;
+import com.org.repository.CandidateRepository;
+import com.org.service.CandidateService;
+
+@Service
+public class CandidateServiceImpl implements CandidateService {
+	
+	@Autowired
+    private CandidateRepository candidateRepo;
+
+
+	public Candidate register(Candidate data) {
+		
+		Candidate c = candidateRepo.save(data);
+		return c;		
+		
+	}
+
+
+}
