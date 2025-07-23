d514c2e89d9887190fafeffc7efc3f05c255a657
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-23 23:04:28 +0530

    Response Tested

--- src/main/java/com/org/controller/HomeController.java
+++ src/main/java/com/org/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.org.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.org.utils.ApiResponse;
 
 @RestController
 public class HomeController {
 	
 	@GetMapping("/test")
-	public String Home() {
-		return "Your Application Running Properly";
+	public ResponseEntity<ApiResponse<String>> Home() {
+		
+		String msg = "Your Application Running Properly";
+		
+		ApiResponse<String> response = new ApiResponse<String>(200,msg,"Good");
+		
+		return ResponseEntity.status(201).body(response);
 	}
 
 }
