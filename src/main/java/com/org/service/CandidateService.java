4b657463a8f63df43c99a4007bb61b6b4a724bfd
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-24 01:24:47 +0530

    Update CandidateService.java

--- src/main/java/com/org/service/CandidateService.java
+++ src/main/java/com/org/service/CandidateService.java
@@ -7,10 +7,4 @@ public interface CandidateService {
 	Candidate register(Candidate data);
 
 	public Candidate viewProfile(Integer id);
-=======
-//<<<<<<< HEAD
-	public Candidate viewProfile(Integer id);
-//=======
-//>>>>>>> 9a98d3943aa4c5495d7a802436b1067199b30f18
->>>>>>> 55fd485d7b6c2f87dfea39f0fcf0850b46c0b1a9
 }
