db60f99d4d28fa9bba6222bdab69d1cc55b623da
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-24 01:13:24 +0530

    Candidate Profile Api Response Changed

--- src/main/java/com/org/controller/CandidateController.java
+++ src/main/java/com/org/controller/CandidateController.java
@@ -3,7 +3,6 @@ package com.org.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,34 +18,27 @@ import com.org.utils.ApiResponse;
 @RestController
 @RequestMapping("/career-portal/candidate")
 public class CandidateController {
-	
+
 	@Autowired
-    private CandidateServiceImpl candidateService;
+	private CandidateServiceImpl candidateService;
 
-	 @PostMapping("/register")
-	    public ResponseEntity<ApiResponse<Candidate>> register(@RequestBody Candidate data) {
-		 
-		 Candidate candidate =  candidateService.register(data);
-		 	
-		 	ApiResponse<Candidate> response = new ApiResponse<Candidate>(200, candidate, "Candidate Registered Succesfully...");
-	        
-		 	return ResponseEntity.status(201).body(response);
-	    }
-	 
-<<<<<<< HEAD
-=======
-//<<<<<<< HEAD
->>>>>>> 55fd485d7b6c2f87dfea39f0fcf0850b46c0b1a9
-	 
-	 @GetMapping("/profile/{id}")
-	 public ResponseEntity<Candidate> profile(@PathVariable Integer id){
-		 return new ResponseEntity<>(candidateService.viewProfile(id), HttpStatus.OK);
-	 }
-<<<<<<< HEAD
+	@PostMapping("/register")
+	public ResponseEntity<ApiResponse<Candidate>> register(@RequestBody Candidate data) {
 
-=======
-	
-//=======
-//>>>>>>> 9a98d3943aa4c5495d7a802436b1067199b30f18
->>>>>>> 55fd485d7b6c2f87dfea39f0fcf0850b46c0b1a9
+		Candidate candidate = candidateService.register(data);
+
+		ApiResponse<Candidate> response = new ApiResponse<Candidate>(200, candidate,"Candidate Registered Succesfully...");
+
+		return ResponseEntity.status(201).body(response);
+	}
+
+	@GetMapping("/profile/{id}")
+	public ResponseEntity<ApiResponse<Candidate>> profile(@PathVariable Integer id) {
+
+		Candidate candidate = candidateService.viewProfile(id);
+
+		ApiResponse<Candidate> response = new ApiResponse<Candidate>(200, candidate,"Candidate Profile Fetched Succesfully...");
+
+		return  ResponseEntity.status(201).body(response);
+	}
 }
--- src/main/java/com/org/serviceImpl/CandidateServiceImpl.java
+++ src/main/java/com/org/serviceImpl/CandidateServiceImpl.java
@@ -23,27 +23,11 @@ public class CandidateServiceImpl implements CandidateService {
 		
 	}
 
-<<<<<<< HEAD
-
-=======
-//
-//<<<<<<< HEAD
->>>>>>> 55fd485d7b6c2f87dfea39f0fcf0850b46c0b1a9
 	@Override
 	public Candidate viewProfile(Integer id) {
+		
 		 return candidateRepo.findById(id).get();
 	}
-<<<<<<< HEAD
-		 
-=======
-	
->>>>>>> 55fd485d7b6c2f87dfea39f0fcf0850b46c0b1a9
-	public List<Candidate> getAllCandidate() {
-		
-		List<Candidate> clist = candidateRepo.findAll();
-		return clist;
-	}
-//>>>>>>> 9a98d3943aa4c5495d7a802436b1067199b30f18
 
 
 }
