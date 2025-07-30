2ea5b9cf7dccfa2ab749c2f89ac388ef9840cfae
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-30 12:56:47 +0530

    Update AdminService.java

--- src/main/java/com/org/service/AdminService.java
+++ src/main/java/com/org/service/AdminService.java
@@ -1,10 +1,15 @@
 package com.org.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.org.entity.Candidate;
 
 public interface AdminService {
+	
+	String refreshAccessToken(String refreshToken);
+	
+	Map<String, String> login(String email, String password);
 	
 	List<Candidate> getAllCandidate();
 
