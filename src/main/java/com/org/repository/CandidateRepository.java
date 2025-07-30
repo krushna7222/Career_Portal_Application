428b686858448bac64ba2d4b7ec92fca01c548d3
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-30 12:14:27 +0530

    Update CandidateRepository.java

--- src/main/java/com/org/repository/CandidateRepository.java
+++ src/main/java/com/org/repository/CandidateRepository.java
@@ -1,9 +1,15 @@
 package com.org.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.org.entity.Candidate;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
 
+	
+	Optional<Candidate> findByEmail(String email);
+
+	
 }
