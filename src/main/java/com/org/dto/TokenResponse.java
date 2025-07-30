79e573fb75d141114395f5912c02f3560c269b42
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-07-30 12:14:54 +0530

    Token Rresponce

---
+++ src/main/java/com/org/dto/TokenResponse.java
@@ -0,0 +1,15 @@
+package com.org.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
+
