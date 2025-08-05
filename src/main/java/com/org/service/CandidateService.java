8d3f0040bc0940346cc3b2d9172a8eab48932520
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-08-05 13:46:33 +0530

    Update CandidateService.java

--- src/main/java/com/org/service/CandidateService.java
+++ src/main/java/com/org/service/CandidateService.java
@@ -1,12 +1,18 @@
 package com.org.service;
 
+import java.util.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.org.entity.Candidate;
 
 import lombok.NonNull;
 
 public interface CandidateService {
 
-	Candidate register(Candidate data);
+	public Candidate register(String name, String email, String password, Long phone, String gender, Date dob,
+            String education, Integer passoutYear, String status, String profileSummary,
+            String workExperience, String skills, MultipartFile resumeFile, MultipartFile photoFile);
 	
 	Candidate findByEmail(@NonNull String email);
 
